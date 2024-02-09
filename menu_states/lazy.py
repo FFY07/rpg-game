319fc9e01d31e780d1214cac1a2bae45db578fa6
FFY07 <fff310277@gmail.com>
2024-02-10 00:07:49 +0800

    seprate module
    Co-authored-by: Heixier <Heixier@users.noreply.github.com>

--- menu_states/lazy.py
+++ menu_states/lazy.py
@@ -6,4 +6,4 @@ def draw_menu():
     sc.screen.fill((0, 0, 0))
     sc.draw_centertext (' ERROR 403 :( ', font.menutitle_font, font.YELLOW,- 200)
     sc.draw_centertext (' sorry, Desmond is lazy to do this feature', font.menu_font, font.WHITE, 0)
-    sc.draw_centertext('and he most likely not going to do this :^) ', font.menu_font, font.WHITE, 40)
+    sc.draw_centertext('and he most likely not going to do this 😉 ', font.menu_font, font.WHITE, 40)
