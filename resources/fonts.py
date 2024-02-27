3f3efa3259249e458a9a90ab7beb35ed504a35f5
Baixier <151296638+Baixier@users.noreply.github.com>
2024-02-27 11:32:57 +0800

    fixed the fonts

--- resources/fonts.py
+++ resources/fonts.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 # fmt: off
 
-eagle_lake = f"{Path('test_zone/resources/fonts/EagleLake/EagleLake-Regular.ttf')}"
-squealer_embossed = f"{Path('test_zone/resources/fonts/Squealer/Squealer_Embossed.otf')}"
-squealer = f"{Path('test_zone/resources/fonts/Squealer/Squealer.otf')}"
+eagle_lake = f"{Path('resources/fonts/EagleLake/EagleLake-Regular.ttf')}"
+squealer_embossed = f"{Path('resources/fonts/Squealer/Squealer_Embossed.otf')}"
+squealer = f"{Path('resources/fonts/Squealer/Squealer.otf')}"
 
-pixeloid_mono = f"{Path('test_zone/resources/fonts/Pixeloid/PixeloidMono.ttf')}"
-pixeloid_sans = f"{Path('test_zone/resources/fonts/Pixeloid/PixeloidSans.ttf')}"
-pixeloid_bold = f"{Path('test_zone/resources/fonts/Pixeloid/PixeloidSans-Bold.ttf')}"
+pixeloid_mono = f"{Path('resources/fonts/Pixeloid/PixeloidMono.ttf')}"
+pixeloid_sans = f"{Path('resources/fonts/Pixeloid/PixeloidSans.ttf')}"
+pixeloid_bold = f"{Path('resources/fonts/Pixeloid/PixeloidSans-Bold.ttf')}"
