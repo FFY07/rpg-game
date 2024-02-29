37f12c0b509e8ea62d15e35f456087431b06b2f9
Heixier <rzsiah@gmail.com>
2024-02-29 16:10:04 +0800

    fixed reaper typo

--- classes/units/reaper.py
+++ classes/units/reaper.py
@@ -108,7 +108,7 @@ class Reaper(Unit):
                 self.melee(target)
                 self.update_stats(target, damage, False, "soul", 3)
 
-                self.play_sound(self.game_audio_handler.sword_sfx)
+                self.play_sound(self.game.audio_handler.sword_sfx)
 
             return True
 
@@ -126,7 +126,7 @@ class Reaper(Unit):
                     damage, crit = self.calc_damage(t, "physical", 0.7)
                     self.update_stats(t, damage, crit, "atk", 2)
 
-                self.play_sound(self.game_audio_andler.sword__sfx)
+                self.play_sound(self.game.audio_handler.sword__sfx)
 
                 return True
 
