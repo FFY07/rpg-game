31cc19dd81899692926ed2d14745c517cd7e90ff
FFY07 <fff310277@gmail.com>
2024-03-04 14:42:05 +0800

    firefire

--- classes/units/marchosias.py
+++ classes/units/marchosias.py
@@ -4,10 +4,10 @@ from classes.unit import Unit
 import gui.ui_functions as ui_functions
 
 # Range of values
-STRENGTH = (15, 15)
-INTELLIGENCE = (18, 18)
+STRENGTH = (0, 0)
+INTELLIGENCE = (40, 40)
 DEFENCE = (80, 80)
-MAGIC_RESIST = (90, 90)
+MAGIC_RESIST = (80, 80)
 race = "Demon"
 
 
@@ -40,9 +40,13 @@ class Marchosias(Unit):
         self.rect.center = self.position
 
         self.move_desc["Passive "] = (
-            "Demon "
+            " Using INT on every skill( include basic attack) "
         )
-
+        
+        self.moves["Hell fire(25)"] = self.hellfire
+        self.move_desc["Hell fire(25 MANA)"] = (
+            "Blasts fire on enemies and burn for 3 turns"
+        )
 
 
     def level_stats(self):
@@ -52,4 +56,44 @@ class Marchosias(Unit):
         self.defence += 5
         self.magic_resist += 2
 
- 
+    def basic_attack(self, target: object, target_team: list):
+
+        if self.is_target_hostile(target):
+
+            damage, crit = self.calc_damage(target, "magic", 0.45)
+                
+            # Melee is optional and only for direct attacks
+            self.melee(target)
+            self.update_stats(target, damage, crit, "misc/physical/slash1", 50)
+
+            # Add mana when attacking
+            if self.mana < self.max_mana:
+                self.mana += 10
+                if self.mana > self.max_mana:
+                    self.mana = self.max_mana
+
+            self.play_sound(self.default_attack_sfx)
+
+            return True
+        
+
+    def hellfire(self, target: object, target_team: list):
+        """Blasts fire on enemies and burn for 3 turns"""
+        if self.is_target_hostile(target):
+            mana_cost = 25
+
+            if self.mana >= mana_cost:
+                self.mana -= mana_cost
+
+                target.burn_stacks.append([3, self.intelligence * 0.375])
+
+                damage, crit = self.calc_damage(target, "magic", 0.4)
+
+                # self.melee(target)
+                self.update_stats(target, damage, crit, "unit/marchosias/fire", 2)
+
+                # Halves defence for 2 turns
+                self.bonus_defence_stacks.append([2, -self.defence / 2])
+                self.game.event_log.append(f"{self.name} use hell fire")
+
+                return True
